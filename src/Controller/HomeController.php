057a2918b0ea122b65f913d98ca24d64295cffa4
Bastien Gondard <bastien.gondard2@gmail.com>
2023-03-03 11:23:54 +0100

    One page with contact form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+
+        $form = $this->createForm(ContactType::class);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'form' => $form
         ]);
     }
 }
